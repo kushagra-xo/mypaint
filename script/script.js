0bd87f5a22af87c08e6057ae148fb6cbbc7324bb
Kushagra J <>
2023-11-01 15:04:05 +0530

    cleaned and improved: no functionality changes

--- script/script.js
+++ script/script.js
@@ -1,37 +1,40 @@
-// paint board configuration
 const SIZE = 16;
 const DEFAULT_COLOR = '#4d4d4d';
 let board = document.getElementById('board');
 
-// config buttons
 let colorInput = document.getElementById('color');
 let reset = document.getElementById('reset');
 let eraserMode = document.getElementById('eraserMode');
 let colorMode = document.getElementById('colorMode');
+let activeMode = document.querySelector('.activeMode');
+let boardCells = document.querySelectorAll('.boardCell');
 
-// default mode 
 let mode = 'color';
 colorMode.classList.add('activeMode');
-let activeMode = document.querySelector('.activeMode');
 
-let current_size = SIZE;
 let current_color = colorInput.value;
 
-// event listeners
-colorInput.addEventListener('input', function() {current_color = this.value;});
-eraserMode.addEventListener('click', function(){
-	mode = 'erase'
-	activeMode.classList.remove('activeMode')
-	this.classList.toggle('activeMode')
+function handleColorInput() {
+  current_color = this.value;
+}
+
+function handleEraserModeClick() {
+  mode = 'erase'
+  activeMode.classList.remove('activeMode')
+  this.classList.toggle('activeMode')
   activeMode = this;
-});
-colorMode.addEventListener('click', function(){
-	mode = 'color'
-	activeMode.classList.remove('activeMode')
-	this.classList.toggle('activeMode')
+}
+
+function handleColorModeClick() {
+  mode = 'color'
+  activeMode.classList.remove('activeMode')
+  this.classList.toggle('activeMode')
   activeMode = this;
-});
-reset.addEventListener('click', resetBoard)
+}
+
+colorInput.addEventListener('input', handleColorInput);
+eraserMode.addEventListener('click', handleEraserModeClick);
+colorMode.addEventListener('click', handleColorModeClick);
 
 function changeColor()
 {
@@ -44,7 +47,6 @@ function changeColor()
 }
 
 function resetBoard() {
-  let boardCells = document.querySelectorAll('.boardCell');
   boardCells.forEach(function(cell) {
     cell.style.backgroundColor = DEFAULT_COLOR;
   });
@@ -52,10 +54,10 @@ function resetBoard() {
 
 function createGrid()
 {
-	board.style.gridTemplateColumns = `repeat(${current_size}, 1fr)`;
-  board.style.gridTemplateRows = `repeat(${current_size}, 1fr)`;
+	board.style.gridTemplateColumns = `repeat(${SIZE}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${SIZE}, 1fr)`;
 
-	for (let i = 0; i < (current_size*current_size) ; i++) 
+	for (let i = 0; i < (SIZE*SIZE) ; i++) 
 	{
 		let boardCell = document.createElement("div");
 		boardCell.classList.add('boardCell');
